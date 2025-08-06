2c46e3a071932a5511e9384b793b5276809ab677
Asjal Amir <113906443+mohdasjal1@users.noreply.github.com>
2025-08-06 19:10:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,14 +36,15 @@ if ingredients_List:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_List:
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        #ingredients_string += fruit_chosen + ' '
-        #ingredients_string += fruit_chosen
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    # st.write(ingredients_string)     
+            ingredients_string += fruit_chosen + ' '
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+            #ingredients_string += fruit_chosen
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+            # st.write(ingredients_string)     
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
